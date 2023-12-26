f90a18c539796d9e8f9a9b9797b91f1145bf5f2c
Ashim Rai <ashrai14691@gmail.com>
2023-12-27 00:22:12 +0545

    updates

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,9 @@ interface Task {
 }
 
 let tasks: Task[] = [];
-
-// ADD TASK
+// --------------------------------------------------------------------------------------------------------------//
+// ---------------------------------------------- ADD TASK ------------------------------------------------------//
+// --------------------------------------------------------------------------------------------------------------//
 function addTask() {
   const newTaskInput = document.getElementById("newTask") as HTMLInputElement;
   const newTaskText = newTaskInput.value.trim();
@@ -17,18 +18,33 @@ function addTask() {
   }
 }
 
-// Enter Key
+//--------------------------- Event listener for the "Add Task" button -------------------------------------//
+const addTaskButton = document.getElementById('addTaskButton');
+if (addTaskButton) {
+  addTaskButton.addEventListener('click', addTask);
+}
+
+// ----------------------------------------------ENTER KEY ------------------------------------------------------//
 document.getElementById("newTask")?.addEventListener("keydown", function (event) {
   if (event.key === "Enter") {
     addTask();
   }
 });
 
-// Complete
-function toggleComplete(index: number) {
+// --------------------------------------------------------------------------------------------------------------//
+// ----------------------------------------------COMPLETED TASK ------------------------------------------------------//
+// --------------------------------------------------------------------------------------------------------------//
+
+function toggleComplete(index: number, event:Event) {
   tasks[index].completed = !tasks[index].completed;
   displayTasks();
+  event.stopPropagation(); // Prevents the click event from propagating to the label
+
 }
+
+// --------------------------------------------------------------------------------------------------------------//
+// ----------------------------------------------DISPLAY TASK ------------------------------------------------------//
+// --------------------------------------------------------------------------------------------------------------//
 function displayTasks(taskArray: Task[] = tasks) {
   const taskList = document.getElementById("taskList");
   if (taskList) {
@@ -41,22 +57,34 @@ function displayTasks(taskArray: Task[] = tasks) {
       return;
     }
 
-    // Add Task
     taskArray.forEach((task, index) => {
       const listItem = document.createElement("li");
-      listItem.innerHTML = `
-        <label class="${task.completed ? 'completed' : ''}">
-          ${task.text}
-          <input type="checkbox" ${task.completed ? "checked" : ""} onchange="toggleComplete(${index})"/>
-          <span class="checkmark"></span>
-        </label>`;
+      const label = document.createElement("label");
+      label.className = task.completed ? 'completed' : '';
+
+      label.innerHTML = `
+      <div class = "listedItem">${task.text}
+      <span class="checkmark"></span></div>
+      `;
+
+      
+      const checkbox = document.createElement("input");
+      checkbox.type = "checkbox";
+      checkbox.className = "checkboxBox";
+      checkbox.checked = task.completed;
+      checkbox.addEventListener("change", (event) => toggleComplete(index, event));
+
+      label.insertBefore(checkbox, label.firstChild);
+      listItem.appendChild(label);
       taskList.appendChild(listItem);
     });
   }
 }
 
+// --------------------------------------------------------------------------------------------------------------//
+// ------------------------------------------------SEARCH--------------------------------------------------------//
+// --------------------------------------------------------------------------------------------------------------//
 
-// SEARCH
 function searchTask() {
   const searchInput = document.getElementById("searchInput") as HTMLInputElement;
   const searchText = searchInput.value.trim().toLowerCase();
@@ -69,35 +97,37 @@ function searchTask() {
 }
 
 
-
-// Event listener for the "Add Task" button
-const addTaskButton = document.getElementById('addTaskButton');
-if (addTaskButton) {
-  addTaskButton.addEventListener('click', addTask);
-}
-
-// Event listener for the search input
+//--------------------------- Event listener for search input -------------------------------------//
 const searchInput = document.getElementById('searchInput');
 if (searchInput) {
   searchInput.addEventListener('input', searchTask);
 }
 
-// Nav buttons
+
+// --------------------------------------------------------------------------------------------------------------//
+// --------------------------------------------NAV BUTTONS--------------------------------------------------------//
+// --------------------------------------------------------------------------------------------------------------//
+
+
+// --------------------------------------------Name Nav--------------------------------------------------------//
 
 document.getElementById("faireNav")?.addEventListener("click", function () {
   displayTasks(tasks);
 });
 
+// --------------------------------------------Completed Nav--------------------------------------------------------//
 document.getElementById("completedNav")?.addEventListener("click", function () {
   const completedTasks = tasks.filter((task) => task.completed);
   displayTasks(completedTasks);
 });
 
+// -----------------------------------------Remaining Nav--------------------------------------------------------//
 document.getElementById("remainingNav")?.addEventListener("click", function () {
   const remainingTasks = tasks.filter((task) => !task.completed);
   displayTasks(remainingTasks);
 });
 
+// -----------------------------------------Home Nav--------------------------------------------------------//
 document.getElementById("homeNav")?.addEventListener("click", function () {
   displayTasks(tasks);
 });
