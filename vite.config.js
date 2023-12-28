3b59afeec29ee0b05e0a2db4622a8043f1a8507e
Ashim Rai <ashrai14691@gmail.com>
2023-12-28 23:32:09 +0545

    updated

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: './', // Sets the base path for the project
-
-  // Other Vite configurations...
+  base: './'
 });
